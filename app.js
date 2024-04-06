25e38d2d2bc946459c9eeb81768aa5c0e71c4446
Prince kushwaha <prince121kk@gmail.com>
2024-04-07 01:17:39 +0530

    Added user authorisation and medical store section

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express=require("express");
+const mysql = require("mysql2");
 const app=express();
 const methodOverride = require("method-override");
 app.use(methodOverride("_method"))
@@ -15,10 +16,92 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'thrivefy',
+    password: 'Prince@102004@'
+});
+
+
+
 app.listen(8080,()=>{
     console.log("server listning from port 8080");
 })
+app.get("/medicines",(req,res)=>{
+    res.send("Login to your account to go to store👤")
+})
+app.get("/medicines/:email",(req,res)=>{
+    let {email}=req.params;
+    console.log(email)
+    let url,username;
+    q = `select * from users where email='${email}'`;
+    try {
+        connection.query(q, (error, result) => {
+            if (error) res.send("user not found");
+            let {url,username,email} = result[0];
+            q = `select * from medicines`;
+            try {
+                connection.query(q, (error, result) => {
+                    if (error) res.send("user not found");
+                    let datas = {result};
+                    let data={username,url,email,datas}
+                    // console.log(userdata)
+                    // res.send(data)
+                    res.render("medicines.ejs", { data });
+                })
+            } catch (error) {
+                console.log("error found");
+            }
+        })
+    } catch (error) {
+        console.log("error found");
+    }
 
+})
+app.get("/signup",(req,res)=>{
+    res.render("signup.ejs");
+})
+app.post("/signup",(req,res)=>{
+    let {username,email,address,password1,password2,url,mobno}=req.body;
+    if (password1 != password2) {
+        res.render("wrongpassword.ejs");
+    } else {
+
+
+        q = `INSERT INTO Users (username, email, address, url, mobno, password) VALUES ('${username}','${email}','${address}','${url}','${mobno}','${password1}')`
+        try {
+            connection.query(q, (error, result) => {
+                if (error) res.send("some error in database" + error.stack);
+                data = { username, email, address, password1 };
+                res.render("user.ejs", { data })
+            })
+        } catch (error) {
+            console.error('error in data base');
+        }
+    }
+})
+app.post("/users",(req,res)=>{
+    let { email, password } = req.body;
+    q = `select * from users where email='${email}'`;
+    try {
+        connection.query(q, (error, result) => {
+            if (error) res.send("user not found");
+            let data = result[0];
+            console.log(data)
+            if (password == data.password) {
+                console.log(data)
+                res.render("user.ejs", { data })
+            } else {
+                res.render("wrongpassword.ejs");
+            }
+
+        })
+    } catch (error) {
+        console.log("error found");
+    }
+})
 
 app.get("/",(req,res)=>{
     res.render("index.ejs");
--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,5 @@
 showloginform = () => {
+    console.log("hello")
     loginform = document.querySelector(".loginform");
     loginform.classList.add("signupformshow")
     close = document.querySelector(".close")
@@ -17,3 +18,22 @@ hideform = () => {
 
 close = document.querySelector(".close")
 close.addEventListener("click", hideform)
+
+function filterCalculators() {
+    var input, filter, calculators, i;
+    input = document.getElementById('medicinesearch');
+    filter = input.value.toUpperCase();
+    calculators = document.querySelectorAll('.medicine');
+
+    for (i = 0; i < calculators.length; i++) {
+        var calculator = calculators[i];
+        var h2 = calculator.querySelector('#medicinename');
+        var calculatorName = h2.textContent || h2.innerText;
+
+        if (calculatorName.toUpperCase().indexOf(filter) > -1) {
+            calculator.style.display="flex";
+        } else {
+            calculator.style.display="none";
+        }
+    }
+}
